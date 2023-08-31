69ea5dc76a09ff543664cb729868c020660bc5d8
Huỳnh Ngọc An <an.huynh@ngs.com.vn>
2023-08-31 16:56:12 +0700

    Test capture api when there is incomming call from 3cx

--- addons/estate/__manifest__.py
+++ addons/estate/__manifest__.py
@@ -7,13 +7,11 @@
     'description': """
     Description text
     """,
-    # data files always loaded at installation
-    # 'data': [
-    #     'views/mymodule_view.xml',
-    # ],
-    # data files containing optionally loaded demonstration data
-    # 'demo': [
-    #     'demo/demo_data.xml',
-    # ],
+    'data': [
+        'security/ir.model.access.csv',
+
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+    ],
     'application': True,
 }
--- addons/estate/models/estate_property.py
+++ addons/estate/models/estate_property.py
@@ -2,7 +2,9 @@ from odoo import fields, models
 
 
 class EstateProperty(models.Model):
-    _name = "estate_property"
+    _name = "estate.property"
     _description = "Estate property model"
 
     name = fields.Char()
+    bedroom = fields.Integer(default=2)
+    availability_date = fields.Date(default=lambda self: fields.Date.today())
